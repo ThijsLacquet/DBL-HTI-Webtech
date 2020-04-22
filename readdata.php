d80778fcbb52d01d569abf6f0408265355948dbf
Thijs Lacquet <thijs.lacquet@gmail.com>
2020-04-23 01:51:50 +0200

    implemented csv parsing
    Added the data provided by the course
    Implemented parsing a csv file and putting it in a mysql database

---
+++ readdata.php
@@ -0,0 +1,58 @@
+<?php
+/*
+	Author: Thijs Lacquet
+*/
+
+set_time_limit(600); //Allows this script to run for a maximum of 5 minutes
+
+//TODO optimize database memory usage
+//Create database and tables if they do not exist yet
+$maxlength_stimuliname = 50;
+$maxlength_user = 50;
+$maxlength_description = 50;
+
+$mysql_connection = new mysqli('localhost:3306', 'root', '');
+
+if (! $mysql_connection->query("CREATE DATABASE IF NOT EXISTS fixationdata")) {
+	die('Could not create mysql database fixationdata: ' . $mysql_connection->error);
+}
+
+if (! $mysql_connection->query("
+	CREATE TABLE IF NOT EXISTS fixationdata.fixationdata(
+	entry_id INT(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	timestamp INT(32) NOT NULL,
+	stimuliname VARCHAR($maxlength_stimuliname) NOT NULL,
+	fixationindex INT(32) NOT NULL,
+	fixationduration INT(32) NOT NULL,
+	mappedfixationpointx INT(32) NOT NULL,
+	mappedfixationpointy INT(32) NOT NULL,
+	user VARCHAR($maxlength_user) NOT NULL,
+	description VARCHAR($maxlength_description) NOT NULL
+	)")) {
+		
+	die('Could not create mysql table fixationdata: ' . $mysql_connection->error);
+}
+
+//Read csv file
+//TODO use user uploaded csv file
+$input = fopen("Provided_data/all_fixation_data_cleaned_up.csv", "r") or die("File could not be opened");
+
+fgets($input); //Skip first line in file
+
+while (!feof($input)) {
+	$line = explode('	', fgets($input));
+	
+	//TODO nice error message if input is in the wrong format
+	//TODO prevent SQL injection
+	if (! $mysql_connection->query("
+		INSERT INTO fixationdata.fixationdata(timestamp, stimuliname, fixationindex, fixationduration,
+		mappedfixationpointx, mappedfixationpointy, user, description)
+		VALUES('$line[0]', '$line[1]', '$line[2]', '$line[3]', '$line[4]', '$line[5]', '$line[6]', '$line[7]')")) {
+			
+		die('sql error: ' . $mysql_connection->error);
+	}
+}
+
+fclose ($input);
+$mysql_connection->close();
+?>
