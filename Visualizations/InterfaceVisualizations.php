b2e9560200d93e6fd13aa8f54ec940ee26aa2b86
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-04-24 10:41:02 +0200

    Implemented strategy interface & concrete strategies
    implementation of the VisualizationStrategy interface. Furthermore, the set-up for two concrete strategies was made.

--- Visualizations/InterfaceVisualizations.php
+++ Visualizations/InterfaceVisualizations.php
@@ -5,4 +5,21 @@
 /*
  *Provides an interface for the visualizations, using the strategy design pattern
  */
- 
+
+interface VisualizationStrategy {
+    public function makeVisualization();
+}
+
+class VisualizationHeatMap implements VisualizationStrategy {
+    public function makeVisualization() {
+        return 1;
+    }
+}
+
+class VisualizationScanpath implements VisualizationStrategy {
+    public function makeVisualization() {
+        return 2;
+    }
+}
+
+?>
