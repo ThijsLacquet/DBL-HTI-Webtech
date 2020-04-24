221a66f90f8f8309882746f072c7649b8b639a94
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-04-24 11:31:41 +0200

    Class Context was added
    class Context was added which exists of the private variable strategy and a choice of which 'algorithm' (thus visualization in this context) to initiate

--- Visualizations/InterfaceVisualizations.php
+++ Visualizations/InterfaceVisualizations.php
@@ -10,16 +10,40 @@ interface VisualizationStrategy {
     public function makeVisualization();
 }
 
-class VisualizationHeatMap implements VisualizationStrategy {
+class Context {
+    private $strategy; // I am doubting if this should be $strategy=NULL (what is normal in PHP)?
+    
+    public function context($choiceStrategy) {
+        switch($choiceStrategy) {
+            case "H":
+                $strategy = new VisualizationHeatmap();
+                break;
+            case "S":
+                $strategy = new VisualizationScanpath();
+                break;
+                /*
+                 * New visualizations need to be added here
+                 */
+        }
+    }
+    
+    $this->strategy->makeVisualization();
+    
+}
+
+class VisualizationHeatmap implements VisualizationStrategy {
     public function makeVisualization() {
         return 1;
+        //this function of course needs to be expanded with all the calculations for this visualization
     }
 }
 
 class VisualizationScanpath implements VisualizationStrategy {
     public function makeVisualization() {
         return 2;
+        //this function of course needs to be expanded with all the calculations for this visualization
     }
+    //New visualizations need to be added here
 }
 
 ?>
