2118c5aa94c667df88efeb14f6321a86c658fcbc
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-05-06 17:36:00 +0200

    updated the read data file with thijs' new code

--- readdata.php
+++ readdata.php
@@ -1,56 +1,68 @@
-<?php
-/*
-	Author: Thijs Lacquet
-*/
-
-require 'sql.php';
-
-set_time_limit(600); //Allows this script to run for a maximum of 5 minutes
-
-//TODO optimize database memory usage
-//Create database and tables if they do not exist yet
-$mysql_connection = connectsql();
-
-if (! $mysql_connection->query("CREATE DATABASE IF NOT EXISTS fixationdata")) {
-	die('Could not create mysql database fixationdata: ' . $mysql_connection->error);
-}
-
-if (! $mysql_connection->query("
-	CREATE TABLE IF NOT EXISTS fixationdata.fixationdata(
-	entry_id INT(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	timestamp INT(32) NOT NULL,
-	stimuliname VARCHAR(50) NOT NULL,
-	fixationindex INT(32) NOT NULL,
-	fixationduration INT(32) NOT NULL,
-	mappedfixationpointx INT(32) NOT NULL,
-	mappedfixationpointy INT(32) NOT NULL,
-	user VARCHAR(50) NOT NULL,
-	description VARCHAR(50) NOT NULL
-	)")) {
+	/*
+	* Reads data from the csv indicated by path and puts it in the SQL database
+	*
+	* @param string $path Path to the csv to be read
+	* @param int $maxlines Maximum number of lines to be read
+	* $param int $document_id Unique document id for the csv. This is added to the database entries
+	* $throws Exception If the mysql query did not succeed
+	* $throws Exception If the file could not be opened
+	*/	
+	public function read($path, $maxlines, $document_id) {		
+		$data = file($path);
 		
-	die('Could not create mysql table fixationdata: ' . $mysql_connection->error);
-}
+		if (!$data) {
+			throw new Exception('File could not be opened');
+		}
+		
+		$n_lines = count($data);
+		
+		//Makes sure that the number of lines won't exceed the maximum number of lines
+		if ($n_lines > $maxlines) {
+			$n_lines = $maxlines;
+		}
+		
+		//Extract lines from data
+		$line = array();
 
-//Read csv file
-//TODO use user uploaded csv file
-$input = fopen("Provided_data/all_fixation_data_cleaned_up.csv", "r") or die("File could not be opened");
-
-fgets($input); //Skip first line in file
-
-while (!feof($input)) {
-	$line = explode('	', fgets($input));
-	
-	//TODO nice error message if input is in the wrong format
-	//TODO prevent SQL injection
-	if (! $mysql_connection->query("
-		INSERT INTO fixationdata.fixationdata(timestamp, stimuliname, fixationindex, fixationduration,
-		mappedfixationpointx, mappedfixationpointy, user, description)
-		VALUES('$line[0]', '$line[1]', '$line[2]', '$line[3]', '$line[4]', '$line[5]', '$line[6]', '$line[7]')")) {
+		for ($i = 0; $i < $n_lines; $i = $i = $i + 1) {
+			$line[$i] = explode('	', $data[$i]);
+		}
+		
+		//Send SQL quaries
+		for ($i = 1; $i < $n_lines; $i = $i + LINESPERQUERY) { 
+			$linestoinsert = LINESPERQUERY;
 			
-		die('sql error: ' . $mysql_connection->error);
+			if ($n_lines - $i < $linestoinsert) {
+				$linestoinsert = $n_lines - $i;
+			}
+			
+			//Build SQL query
+			$query = 'INSERT INTO fixationdata.fixationdata(document_id, timestamp, stimuliname, fixationindex, fixationduration,
+				mappedfixationpointx, mappedfixationpointy, user, description)VALUES';
+			
+			for ($offset = 0; $offset < $linestoinsert; $offset = $offset + 1) {
+				$query .= "({$document_id}, ";
+				
+				for ($column = 0; $column < 8; $column = $column + 1) {
+					$query .= "'";
+					$query .= $line[$i + $offset][$column];
+					$query .= "'";
+					if ($column != 7) {
+						$query .= ',';
+					}
+				}
+				$query .= ')';
+				
+				if ($offset != $linestoinsert - 1) {
+					$query .= ',';
+				}
+			}	
+			
+			//Send SQL query
+			if (! $this->mysql_connection->query($query)) {
+				throw new Exception('sql error: ' . $this->mysql_connection->error);
+			}
+		}
 	}
 }
-
-fclose ($input);
-$mysql_connection->close();
 ?>
