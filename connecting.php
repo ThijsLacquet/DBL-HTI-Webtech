8aeaeb01cf5231e16bf49efa87d5cb0f9368533a
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-05-06 17:32:35 +0200

    add sql query

--- connecting.php
+++ connecting.php
@@ -3,6 +3,16 @@
 require 'sql.php';
 $mysql_connection = connectsql();
 
+$sql = "
+	SELECT *
+	FROM
+	(SELECT *, LAG(AOI) OVER(PARTITION BY user ORDER BY timestamp) prevAOI
+	FROM
+	(SELECT user,
+	timestamp - MIN(timestamp) OVER(PARTITION BY user) AS timestamp,
+		CASE
+";
 
+$result = mysqli_query($dbc, $sql) or die("Bad Query: $sql");
 
 ?>
