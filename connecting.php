172955a8b23a391546ed82eaad19d32675cf58e2
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-05-07 10:43:22 +0200

    added working sql query
    to select fixationduration, pointx, pointy with the stimulus name

--- connecting.php
+++ connecting.php
@@ -1,18 +1,28 @@
 <?php
-
+# author: Marleen van Gent
 require 'sql.php';
 $mysql_connection = connectsql();
 
-$sql = "
-	SELECT *
-	FROM
-	(SELECT *, LAG(AOI) OVER(PARTITION BY user ORDER BY timestamp) prevAOI
-	FROM
-	(SELECT user,
-	timestamp - MIN(timestamp) OVER(PARTITION BY user) AS timestamp,
-		CASE
-";
+if ($mysql_connection->connect_error) {
+    die("Failed to connect to mysql: " . $mysql_connection->connect_error);
+}
 
-$result = mysqli_query($dbc, $sql) or die("Bad Query: $sql");
+# $sql = "SELECT * FROM fixationdata.fixationdata";
+$sql = "SELECT fixationduration, mappedfixationpointx, mappedfixationpointy FROM fixationdata.fixationdata WHERE stimuliname = '01_Antwerpen_S1.jpg'";
+$result = $mysql_connection->query($sql);
+
+if (!$result) {
+    trigger_error('Invalid query: ' . $mysql_connection->error);
+}
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "fixationduration: " . $row["fixationduration"]. " - X: " . $row["mappedfixationpointx"]. " - Y: " . $row["mappedfixationpointy"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+# mysqli_result object converteren naar array
 
 ?>
