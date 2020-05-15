31844b1b7cab021fb4f15019824177698e525e49
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-05-15 08:58:08 +0200

    added readdataexample

---
+++ readdataexample.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Author: Thijs Lacquet
+*/
+require 'readdata.php';
+
+$readdata = new Readdata;
+
+$readdata->read("C:\Users\vglee\Documents\GitHub\DBL-HTI-Webtech\Provided_data", 200000, 1);
+unset($readddata);
+
+?>
