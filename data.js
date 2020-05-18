711e659b90be9d8457b5a7b87cb2b6ad5eb9fa27
NathanOverschie <64199289+NathanOverschie@users.noreply.github.com>
2020-05-18 16:34:47 +0200

    comments added

--- data.js
+++ data.js
@@ -1,5 +1,9 @@
+//By Nathan
 class data {
 
+	//the constuctor makes a new instance of this data object
+	//Parameters:
+	//	stimuliname: the name of the picture and the corresponding fixationdata
 	constructor(stimuliname){
 		var array = null;
 		var superThis = this;
@@ -18,6 +22,9 @@ class data {
 		});
 	}
 
+	//a function that is called to interpret the data from the server and add it to this data structure
+	//Parameters:
+	//	array: this is the JSON parsed output of the file connecting.php
 	interpret(array){
 		var currentUser;
 		var currentName = null;
@@ -47,6 +54,11 @@ class data {
 		return this.AOIs;
 	}
 
+
+	//this function selects the users that you want to be selected
+	//Parameters:
+	//	users: either a name of a user (string) or a array of names
+	//	append: a bool, that determinds if the users will be append to the current selection, or if the selection will be reset
 	selectUsers(users, append){
 		if(typeof(users) == "object"){
 			for(var i=0;i<this.numofUsers;i++){
@@ -74,7 +86,71 @@ class data {
 			}
 		}
 	}
+	//this filters the data base on the function parameter
+	//Parameters:
+	//	func: a function with the follow requirements:
+	//		Parameters:
+	//			class dataEntry: the entry that can either by filtered out or not 
+	//		Return value:
+	//			bool: weither it is filtered out or not (true => it stays in the data, false => it is removed from the data)
+	filter(func){
+		for(var i=0;i<this.numofUsers;i++){
+			for(var j=0;j<this.users[i].numofEntries;j++){
+				if(func(this.users[i].entries[j])){
+					this.users[i].entries[j].enabled = true;
+				}else{
+					this.users[i].entries[j].enabled = false;
+				}
+			}
+		}
+	}
 
+	//this resets the filter
+	resetfilter(){
+		for(var i=0;i<this.numofUsers;i++){
+			this.users[i].enabled = true;
+			for(var j=0;j<this.users[i].numofEntries;j++){
+				this.users[i].entries[j].enabled = true;
+			}
+		}
+	}
+
+	//filters the data on the time
+	//Parameters:
+	//	min: the minimal time
+	//	max: the maximal time
+	timeRange(min, max){
+		this.filter(function(x){
+			if((min < x.time) && (x.time < max)){
+				return true;
+			}else{
+				return false;
+			}
+		});
+	}
+
+	//filters the data on the duration
+	//Parameters:
+	//	min: the minimal duration
+	//	max: the maximal duration
+	durationRange(min, max){
+		this.filter(function(x){
+			if((min < x.duration) && (x.duration < max)){
+				return true;
+			}else{
+				return false;
+			}
+		});
+	}
+
+	//This return the data in the format the heatmap can use
+	//Parameters:
+	//	filtered: a bool, true gives the filtered data, false gives all the data
+	//Return value:
+	//	[x, y, dt]
+	//		x: an array of x-coordinates
+	//		y: an array of y-coordinates
+	//		dt: an array of durations
 	toHeatmapformat(filtered){
 		var x = [];
 		var y = [];
