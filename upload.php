55a534524c4e402582fe7a29a49881237d23380a
marleeenvg <64195590+marleeenvg@users.noreply.github.com>
2020-05-25 09:50:26 +0200

    made upload tool work correctly

--- upload.php
+++ upload.php
@@ -5,8 +5,8 @@
 include_once __DIR__.'/readdata.php';
 include_once __DIR__.'/user.php';
 
-$dir = dirname(__FILE__)."/CSV/";
-$expire = 24 * 3600; //Time before cookie/user expires in seconds; currently 1 day
+$dir = dirname(__FILE__).'/../data/';
+$expire = 24 * 3600; //Time before cookie/user expires in seconds; currently 1 day;
 
 //Establish user
 $myUser = new User($dir);
